29fe218404db8bf6ce7222f1c2dbf5f6ee3919dd
Appiah samuel <92890571+sappiah085@users.noreply.github.com>
2021-12-11 10:38:09 +0000

    new

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const menu = document.querySelector(".navlinks");
+const ul = document.querySelector(".nav");
 function my() {
-menu.classList.toggle("active");
+    menu.classList.toggle("active");
+    if(menu.classList.contains("active")){
+        setTimeout(()=>{ul.style.display = "block"} ,500)
+    }
+    else{
+        setTimeout(()=>{ul.style.display = "none"} ,200)
+        
+    }
+        
+
 }
